2f2feaa45a62ce3815500f50a3784fb2fa32d588
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-03-14 13:26:54 -0300

    feat(channels.scm): add channels

---
+++ guix/channels.scm
@@ -0,0 +1,11 @@
+(list (channel
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (branch "master")
+       (commit
+        "678b3dddfe442e643fe5cff7730d4f9690c3e2c2")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
