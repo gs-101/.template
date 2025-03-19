6a0c04bc3657ab4f80f94a5d05597f25ebc7645c
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-03-19 10:20:44 -0300

    refactor(flake.nix): drop depencendy on flake-utils
    Drop the dependency on flake-utils by using a simple function to
    define the systems.

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,25 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-  flake-utils.lib.eachDefaultSystem (
-    system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
+        inherit system;
+        pkgs = nixpkgs.legacyPackages.${system};
+      });
 
     in {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          git
-        ];
-      };
-    }
-  );
+      devShells = forAllSystems ({ pkgs, ... }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            git
+          ];
+        };
+      });
+    };
 }
